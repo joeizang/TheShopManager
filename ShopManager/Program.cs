1b29825d877cff954b51bdc62676a69ce26cd18b
Joseph Izang <josephizang@gmail.com>
2024-11-05 23:50:27 +0100

    cleanup demo code. add api auth

--- ShopManager/Program.cs
+++ ShopManager/Program.cs
@@ -25,7 +25,6 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-app.MapIdentityApi<ApplicationUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -34,25 +33,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapIdentityApi<ApplicationUser>();
 
 app.Run();
 
