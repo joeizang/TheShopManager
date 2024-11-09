b9850f0e57d8e59e698a82c5b6adce12f1573e19
Joseph Izang <josephizang@gmail.com>
2024-11-09 10:15:27 +0100

    clean up

--- ShopManager/Program.cs
+++ ShopManager/Program.cs
@@ -40,8 +40,3 @@ app.UseHttpsRedirection();
 app.MapIdentityApi<ApplicationUser>();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
