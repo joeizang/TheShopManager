dfcc05cbfedcbf606198cd4cb4ac7fc468d7d2f4
Joseph Izang <josephizang@gmail.com>
2024-11-11 09:41:58 +0100

    updates to shop command

--- ShopManager/Features/Shops/ShopsCommandService.cs
+++ ShopManager/Features/Shops/ShopsCommandService.cs
@@ -2,7 +2,10 @@ using System;
 
 namespace ShopManager.Features.Shops;
 
-public class ShopsCommandService
+public class ShopsCommandService : IShopCommandService
 {
-
+    public Task<ShopDto> CreateShop(CreateShopDto model)
+    {
+        throw new NotImplementedException();
+    }
 }
--- ShopManager/Features/Shops/ShopsDto.cs
+++ ShopManager/Features/Shops/ShopsDto.cs
@@ -1,3 +1,5 @@
 namespace ShopManager.Features.Shops;
 
 public record ShopDto(string shopName, string shopPhoneNumber, bool shopStatus, string shopAddress, Guid shopId);
+
+public record CreateShopDto(string shopName, string shopPhoneNumber, string shopAddress);
